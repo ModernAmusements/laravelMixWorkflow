65f61bc6dbb2ed70b6c028a5d05bb0c8311afb9e
ModernAmusements <st@zahnarzt-helden.de>
2020-07-21 13:10:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# mkdir my-app && cd my-app
+# npm init -y
+# npm install laravel-mix --save-dev
+# cp node_modules/laravel-mix/setup/webpack.mix.js ./
+
+# node_modules/
+# package.json
+# webpack.mix.js
+
+# Head over to your webpack.mix.js file:
+
+# const mix = require('laravel-mix');
+
+# mix.js('src/app.js', 'dist')
+   .sass('src/app.scss', 'dist')
+   .setPublicPath('dist');
+
+# mkdir src && touch src/app.{js,scss}
+
+# Compile everything down by running:
+# node_modules/.bin/webpack --config=node_modules/laravel-mix/setup/webpack.config.js
+
+# dist/app.css
+# dist/app.js
+# dist/mix-manifest.json (Your asset dump file, which we'll discuss later.)
+# Nice job! Now get to work on that project.
+
+
+# npm install postcss-css-variables --save-dev
+
+
+
+# node_modules/.bin/webpack --config=node_modules/laravel-mix/setup/webpack.config.js
+# laravelMixWorkflow
---
+++ src/js/main-type.js
@@ -0,0 +1,58 @@
+let wght2 = 800;
+let slnt2 = 0;
+let ital2 = 0.5;
+document
+    .getElementById("fontSizeSlider")
+    .addEventListener("input", function () {
+        if (window.innerWidth > 767.98) {
+            document.getElementById("controls-result").style.fontSize =
+                document.getElementById("fontSizeSlider").value + "vw";
+            document.getElementById("fontSizeResult").innerHTML =
+                document.getElementById("fontSizeSlider").value + "vw";
+        } else {
+            document.getElementById("controls-result").style.fontSize =
+                document.getElementById("fontSizeSlider").value * 2 + "vw";
+            document.getElementById("fontSizeResultSm").innerHTML =
+                Math.floor(document.getElementById("fontSizeSlider").value * 2) + "vw";
+        }
+    });
+
+
+document.getElementById("wghtSlider").addEventListener("input", function () {
+    wght2 = document.getElementById("wghtSlider").value;
+    document.getElementById(
+        "controls-result"
+    ).style.fontVariationSettings = `'wght' ${wght2}, 'slnt' ${slnt2}, 'CRSV' ${ital2}`;
+    document.getElementById("wghtResult").innerHTML = document.getElementById(
+        "wghtSlider"
+    ).value;
+});
+
+
+document.getElementById("slntSlider").addEventListener("input", function () {
+    slnt2 = document.getElementById("slntSlider").value;
+    document.getElementById(
+        "controls-result"
+    ).style.fontVariationSettings = `'wght' ${wght2}, 'slnt' ${slnt2}, 'CRSV' ${ital2}`;
+    document.getElementById("slntResult").innerHTML = document.getElementById(
+        "slntSlider"
+    ).value;
+});
+
+
+
+var slider = document.getElementById("range1");
+var output = document.getElementById("slider2");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+var slider = document.getElementById("range2");
+var output = document.getElementById("slider1");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
